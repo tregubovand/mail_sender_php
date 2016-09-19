cb1140a31444aaf3b6990d304f5951eccdb887c2
Tregubov Andrey <tregubovand@gmail.com>
2016-09-19 14:00:10 +0300

    main include file

---
+++ mail.lib.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* Отправка HTML писем При инициализации передать массив с ключами to, subject, massage, nameto, namefrom, mailfrom
+*/
+class sendMail
+{
+  function __construct($data)
+  {
+    $to  = $data['to'];
+    $subject = $data['subject'];
+    $message = $data['massage'];
+    $name = $data['nameto'];
+    $from = $data['namefrom'];
+    $mailfrom = $data['mailfrom'];
+
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= 'To: '.$name.' <'.$data['to'].'>' . "\r\n";
+    $headers .= 'From: '.$from.' <birthday@example.com>' . "\r\n";
+
+    // Отправляем
+    mail($to, $subject, $message, $headers);
+  }
+}
+
+?>
