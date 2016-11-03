5e39400bedf861571f477daea543b4b18ea83be5
Tregubov Andrey <tregubovand@gmail.com>
2016-11-03 14:47:24 +0300

    Update mail.lib.php

--- mail.lib.php
+++ mail.lib.php
@@ -16,7 +16,7 @@ class sendMail
     $headers  = 'MIME-Version: 1.0' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
     $headers .= 'To: '.$name.' <'.$data['to'].'>' . "\r\n";
-    $headers .= 'From: '.$from.' <birthday@example.com>' . "\r\n";
+    $headers .= 'From: '.$from.' <'.$mailfrom.'>' . "\r\n";
 
     // Отправляем
     mail($to, $subject, $message, $headers);
