5691e098699d97c2e135f16d0b38fcdf74d644ed
Tregubov Andrey <tregubovand@gmail.com>
2016-12-22 04:20:36 +0800

    Изменил чарсет

--- mail.lib.php
+++ mail.lib.php
@@ -14,7 +14,7 @@ class sendMail
     $mailfrom = $data['mailfrom'];
 
     $headers  = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
     $headers .= 'To: '.$name.' <'.$data['to'].'>' . "\r\n";
     $headers .= 'From: '.$from.' <'.$mailfrom.'>' . "\r\n";
 
